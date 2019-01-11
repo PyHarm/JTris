0b5a40f9c8e21941a92e09bc74347ea1e3816855
PyHarm <42048367+PyHarm@users.noreply.github.com>
2019-01-11 17:22:41 +0100

    GUI V1

--- Jtrisjs.js
+++ Jtrisjs.js
@@ -1,5 +1,5 @@
 var ctx = document.querySelector("canvas");
-ctx.width = window.innerWidth/2 - 20;
+ctx.width = window.innerWidth/10*3 - 20;
 ctx.height = window.innerHeight - 20;
 var canv = ctx.getContext("2d");
 
@@ -17,12 +17,12 @@ function main(){
 }
 
 function update(){
-
+    console.log(width);
 }
 
 function draw(){
     canv.fillStyle = "rgb(255,255,0)";
-    canv.fillRect(70,50,200,200);
+    canv.fillRect(0,0,200,200);
 }
 
 setup();
